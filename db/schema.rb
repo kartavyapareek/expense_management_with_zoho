9aac8e391a9c771f811a230907083316e0eb65b5
Kartavya Pareek <pareekkartavya@gmail.com>
2024-04-07 14:04:43 +0530

    feat: cleanup done and rubocop rspec and sidekiq added

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,4 +13,5 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 0) do
+  # will add migrations here
 end
